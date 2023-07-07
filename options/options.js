f30c3bd04f0973e70b752eb8b6b2fd71ecf9bad0
Crowdbotics <team@crowdbotics.com>
2023-07-07 20:53:46 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "safari-time-tracker-41990",
+  url: "https://safari-time-tracker-41990.botics.co",
+  api: "https://safari-time-tracker-41990.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
